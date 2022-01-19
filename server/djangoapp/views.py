2d6a8dc93462fcc33973d091b705857009b8b06d
sbj3 <broadus.jones@gmail.com>
2022-01-19 20:01:43 +0000

    Signup login and logout working in test

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -61,8 +61,39 @@ def logout_request(request):
 
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    context={}
+    # If it is a GET request, just render the registration page
+    if request.method == "GET":
+        return render(request, 'djangoapp/registration.html', context)
+    # If it is a POST request
+    elif request.method == "POST":
+        # Get user information from request.POST
+        # username, first_name, last_name, password
+        username =  request.POST['username']
+        first_name =  request.POST['firstname']
+        last_name =  request.POST['lastname']
+        password =  request.POST['psw']
+        user_exist = False
+        try:
+            # Check if user already exists
+            User.objects.get(username=username)
+            user_exists = True
+        except:
+            # If not, simply log this is a new user
+            logger.debug("{} is a new user".format(username))
+        if not user_exist:
+            # Create user in austh_user table
+            user = User.objects.create_user(
+                username=username, password=password, 
+                first_name=first_name, last_name=last_name)
+            # Login the user and
+            # redirect to main page
+            login(request, user)
+            return render(request, 'djangoapp/index.html', context)
+        else:
+            return render(request, 'djangoapp/registration.html', context)
+
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
