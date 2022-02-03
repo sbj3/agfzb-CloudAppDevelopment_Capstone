e7fed603013010c1a85ff2716ce733ff01f3cb6b
Broadus Jones <broadus.jones@gmail.com>
2022-02-03 10:08:24 -0600

    Added dealership api on cloudant.

---
+++ functions/api_dealership.js
@@ -0,0 +1,79 @@
+/**
+ * Get all dealerships
+ */
+
+ const Cloudant = require('@cloudant/cloudant');
+
+
+ function main(params) {
+    console.log(params);
+
+    return new Promise(function (resolve, reject) {
+        const cloudant = Cloudant({
+            url: params.COUCH_URL,
+            plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
+        });
+
+        const dealershipDb = cloudant.use('dealerships');
+
+        if (params.state) {
+            // return dealership with this state
+
+            dealershipDb.find({
+                "selector": { "state": { "$eq": params.state
+                } } }, function (err, result) {
+                if (err) {
+                   console.log("file: api_dealership.js ~ find state err", err)
+                   reject(err);
+                }
+
+                let code = 200;
+                if (result.docs.length==0) {
+                   code = 404;
+                }
+
+                resolve({
+                   statusCode: code,
+                   headers: { 'Content-Type': 'application/json' },
+                   body: result
+                });
+            });
+        } else if (params.id) {
+            id = parseInt(params.dealerId)
+            // return dealership
+            dealershipDb.find({
+                selector: {id:parseInt(params.id)}}, function (err, result) {
+
+               if (err) {
+                   console.log("file: api_dealership.js ~ find dealership err", err)
+                   reject(err);
+               }
+
+               let code = 200;
+               if (result.docs.length==0) {
+                   code = 404;
+               }
+               resolve({
+                   statusCode: code,
+                   headers: { 'Content-Type': 'application/json' },
+                   body: result
+               });
+           });
+        } else {
+            // return all dealerships
+            dealershipDb.list({ include_docs: true }, function (err, result) {
+
+               if (err) {
+                   console.log("file: api_dealership.js ~ all docs err", err)
+                   reject(err);
+               }
+
+               resolve({
+                   statusCode: 200,
+                   headers: { 'Content-Type': 'application/json' },
+                   body: result
+               });
+           });
+        }
+    });
+}
