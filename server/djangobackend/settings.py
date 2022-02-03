b033e70e8bb71b980183442e4590899d141ba6c7
Broadus Jones <broadus.jones@gmail.com>
2022-02-03 14:38:36 -0600

    Reset database to sqlite.

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'djangobackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'MTU1NDgtYnJvYWR1',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
