69ef45a341174fcb0fd0e502e270744ac3bc5ba4
sbj3 <broadus.jones@gmail.com>
2022-02-04 19:38:36 +0000

    Connected to dealership api

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -31,9 +31,8 @@ def contact(request):
     if request.method == 'GET':
         return render(request, 'djangoapp/contact.html', context)
 
+
 # Create a `login_request` view to handle sign in request
-
-
 def login_request(request):
     context = {}
     if request.method == 'POST':
@@ -100,11 +99,19 @@ def registration_request(request):
 
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
+# def get_dealerships(request):
+#     context = {}
+#     if request.method == "GET":
+#         return render(request, 'djangoapp/index.html', context)
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
-
+        url = "https://us-south.functions.appdomain.cloud/api/v1/web/broadus.jones%40gmail.com_dev/api/dealership"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
