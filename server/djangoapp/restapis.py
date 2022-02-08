8b5782763df37cab65af5edc1d5376039a4ba599
Broadus Jones <broadus.jones@gmail.com>
2022-02-08 13:35:21 -0600

    Minor formatting from linter warnings.

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -6,7 +6,7 @@ from requests.auth import HTTPBasicAuth
 
 
 # Create a `get_request` to make HTTP GET requests
-# e.g., response = requests.get(url, params=params, 
+# e.g., response = requests.get(url, params=params,
 #                               headers={'Content-Type': 'application/json'},
 #                               auth=HTTPBasicAuth('apikey', api_key))
 def get_request(url, **kwargs):
@@ -14,10 +14,10 @@ def get_request(url, **kwargs):
     print("GET from {} ".format(url))
     try:
         # Call get method of requests library with URL and parameters
-        response = requests.get( 
-            url, 
+        response = requests.get(
+            url,
             headers={'Content-Type': 'application/json'},
-            params=kwargs)  
+            params=kwargs)
     except:
         # If any error occurs
         print("Network exception occurred")
@@ -47,15 +47,15 @@ def get_dealers_from_cf(url, **kwargs):
         for dealer in dealers:
             # Get its content in `doc` object
             dealer_doc = dealer["doc"]
-            dealer_id  = dealer_doc.get("_id")
+            dealer_id = dealer_doc.get("_id")
             if dealer_id[:7] == '_design':
                 continue
             # print(dealer_doc)
             # Create a CarDealer object with values in `doc` object
-            
+
             dealer_obj = CarDealer(
-                address=dealer_doc["address"], city=dealer_doc["city"], 
-                full_name=dealer_doc["full_name"], id=dealer_doc["id"], 
+                address=dealer_doc["address"], city=dealer_doc["city"],
+                full_name=dealer_doc["full_name"], id=dealer_doc["id"],
                 lat=dealer_doc["lat"], long=dealer_doc["long"],
                 short_name=dealer_doc["short_name"],
                 st=dealer_doc["st"], zip=dealer_doc["zip"])
@@ -63,16 +63,15 @@ def get_dealers_from_cf(url, **kwargs):
 
     return results
 
-# Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
+# Create a get_dealer_reviews_from_cf method to get reviews by dealer id from
+# a cloud function
 # def get_dealer_by_id_from_cf(url, dealerId):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a DealerView object list
 
 
-# Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
+# Create an `analyze_review_sentiments` method to call Watson NLU and
+# analyze text
 # def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
-
-
-
